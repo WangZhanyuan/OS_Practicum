c70a165466c0be5a2f94ae5508df581d12a760d2
Baymax <wangzhanyuan@hotmail.com>
2018-07-05 16:57:35 +0800

    submit

---
+++ iNode.cpp
@@ -0,0 +1,4 @@
+struct iNode{
+  unsigned int ino;
+  
+}
---
+++ login.cpp
@@ -0,0 +1,10 @@
+#include "log.h"
+
+using namespace std;
+
+main(){
+  
+  //cout << "test" << endl;
+  //system("pause");
+  return 0;
+}
